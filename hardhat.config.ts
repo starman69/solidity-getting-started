e38069e72e66bf51d2bf9a90d94e08a769d1e68d
starman69 <dave@pattens.org>
2025-01-14 18:54:34 -0800

    add hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+        url: process.env.ALCHEMY_API_URL,
+        accounts: [`0x${process.env.SEPOLIA_PRIVATE_KEY}`],
+    }
+  }
+};
+
+export default config;
