2b8f719df213ee0166b84aae3f2fc00544301b78
starman69 <dave@pattens.org>
2025-01-15 19:35:05 -0800

    update default config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,11 @@ import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {
+      chainId: 31337, // Local Hardhat chain ID
+    },
     localhost: {
       url: "http://127.0.0.1:8545",
     },
